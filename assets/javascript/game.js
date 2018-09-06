7abf85060d47915bea34996f4fd74514a5d7ce22
LRKirkendoll <l.r.kirkendoll@gmail.com>
2018-09-05 22:21:23 -0500

    pseudo

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+// Random number generated at the beginning of the game //
+// Four crystals with hidden, randomized values between 1 - 12 //
+// When the crystal is click the value of the crystal will be added to the player's score //
+// The game will continue until the following conditions are met://
+// If the player's score is equal to the random number generated, then the "Wins" counter increases by 1//
+// If the player's score is greater than the random number generated, then the "Losses" counter increases by 1//
+// Once one of the two conditions is met, then the game resets with a new randomly generated number  and crystal values//
