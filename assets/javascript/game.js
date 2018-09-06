92f9d0dd9d69ffbcba02c2c58a983102cc4f9134
LRKirkendoll <l.r.kirkendoll@gmail.com>
2018-09-06 14:41:46 -0500

    draft

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,13 @@
 // Random number generated at the beginning of the game 19 - 120//
 var randomNumber;
+var Wins = 0;
+var Losses = 0;
+var runningTotal = 0;
+
+randomNumber = Math.floor(Math.random() * 101 ) + 19;
+console.log (randomNumber);
+
+$("#random").html("Target Number: " + randomNumber);
 
 
 
@@ -19,6 +27,24 @@ for (var i = 0; i < 4; i++) {
 
 }
 // When the crystal is click the value of the crystal will be added to the player's score //
+$(".crystal").on('click', function () {
+    
+    var number = parseInt($(this).attr('data-random'));
+
+    runningTotal += number;
+
+    console.log(runningTotal)
+
+    if(runningTotal > randomNumber) {
+        console.log("You lost!");
+    }
+    else if (runningTotal===randomNumber){
+        console.log("You won!!!");
+    }
+    
+
+});
+    
 // The game will continue until the following conditions are met://
 // If the player's score is equal to the random number generated, then the "Wins" counter increases by 1//
 // If the player's score is greater than the random number generated, then the "Losses" counter increases by 1//
